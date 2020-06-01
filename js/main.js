7b1f5e261e61bb555f8ad0fb46737606c44096c3
Silvia Eveline <evelineborges1992@gmail.com>
2020-06-01 10:56:13 -0300

    Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
+//Aula 01
+
 //alert("meu primeiro js");
-var nome = "Silvia Eveline";
-var idade = 20;
-var idade2 = 7;
-var frase = "Japao é o melhor time do mundo"
+//var nome = "Silvia Eveline";
+//var idade = 20;
+//var idade2 = 7;
+//var frase = "Japao é o melhor time do mundo"
 //alert(nome + "tem" + idade + "anos");
 //alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(idade * idade2);
-console.log(frase.replace("Japao", "Brasil"));
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(idade * idade2);
+//console.log(frase.replace("Japao", "Brasil"));
 //alert(frase.replace("Japão", "Brasil"));
 
 
+//Aula 02
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva"); //inclui
+//lista.pop(); //tira item da lista
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join('-'));
+
+//dicionario
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta);
+
+//lista de dicionario
+
+//var fruta = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxo"} ]
+//console.log(fruta);
+//alert(fruta[1].nome);
+	
 
