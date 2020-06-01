d91f9619513c33bc12cf29ac2a4589d45ef908da
Silvia Eveline <evelineborges1992@gmail.com>
2020-06-01 12:04:14 -0300

    Funções

--- js/main.js
+++ js/main.js
@@ -66,3 +66,34 @@ var d = new Date();
 alert(d.getMonth()+1);
 */
 
+//Aula 04
+/*
+function soma(n1, n2){
+	return n1 + n2;
+}
+
+alert(soma(5, 10));
+
+function setReplace(frase, nome, novo_nome){
+	return frase.replace(nome, novo_nome);
+}
+
+alert(setReplace("Vai Japao", "Japao", "Brasil"));
+*/
+/*
+function validadeIdade(idade){
+	var validar;
+	if(idade >= 18){
+		validar = true
+	}else{
+		validar = false
+	}
+	return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validadeIdade(idade));
+*/
+
+
+
