a1407044df2bde63cd9aa971d00844a401e59f96
Silvia Eveline <evelineborges1992@gmail.com>
2020-05-31 23:23:24 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//alert("meu primeiro js");
+var nome = "Silvia Eveline";
+var idade = 20;
+var idade2 = 7;
+var frase = "Japao é o melhor time do mundo"
+//alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(idade * idade2);
+console.log(frase.replace("Japao", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+
+
+
